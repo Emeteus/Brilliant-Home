75c8c8051eb0b8d29ac8dec090877f414db36bf2
Emeteus <abodemaxmod2002@gmail.com>
2024-01-23 22:45:49 +0200

    language update

---
+++ js/langarr.js
@@ -0,0 +1,166 @@
+const langArr = {
+    "chooseDesign": {
+        "en": "Choose the design you like best",
+        "ua": "Оберіть дизайн, який вам подобається найбільше",
+    },
+    "under": {
+        "en": "Site where your home dream will come true",
+        "ua": "Сайт, де здійсниться ваша мрія про будинок",
+    },
+    "login": {
+        "en": "Login",
+        "ua": "Логін",
+    },
+    "register": {
+        "en": "Register",
+        "ua": "Зареєструватись",
+    },
+    "watch": {
+        "en": "Watch",
+        "ua": "Дивитися",
+    },
+    "bohemian":{
+        "en": "Bohemian design",
+        "ua": "Богемний дизайн",
+    },
+    "eclectic":{
+        "en": "Eclectic design",
+        "ua": "Еклектичний дизайн",
+    },
+    "industrial":{
+        "en": "Industrial design",
+        "ua": "Індустріальний дизайн",
+    },
+    "modern":{
+        "en": "Modern design",
+        "ua": "Сучасний дизайн",
+    },
+    "scandinavian":{
+        "en": "Scandinavian design",
+        "ua": "Скандинавський дизайн",
+    },
+    "more":{
+        "en": "More",
+        "ua": "Більше",
+    },
+
+    "bohemianInterior":{
+        "en": "Bohemian Design Interior",
+        "ua": "Богемний дизайн інтер'єру",
+    },
+    "electricInterior":{
+        "en": "Eclectic Design Interior",
+        "ua": "Еклектичний дизайн інтер'єру",
+    },
+    "industrialInterior":{
+        "en": "Industrial Design Interior",
+        "ua": "Індустріальний дизайн інтер'єру",
+    },
+    "ModernInterior":{
+        "en": "Modern Design Interior",
+        "ua": "Сучасний дизайн інтер'єру",
+    },
+    "scandinavianInterior":{
+        "en": "Scandinavian Design Interior",
+        "ua": "Скандинавський дизайн інтер'єру",
+    },
+    "close":{
+        "en": "Close",
+        "ua": "Закрити",
+    },
+    "bohemianText":{
+        "en": "Bohemian interior design is a style that embodies openness to diversity and creativity. The basis of this direction is the rejection of standard rules and a free combination of different styles and materials. A room in the bohemian style is often distinguished by the use of bright colors, natural materials, as well as various textile products.",
+
+        "ua": "Богемний дизайн інтер'єру – це стиль, що втілює в собі відкриття до різноманітності та творчість. В основі цього напряму лежить відмова від стандартних правил та вільне поєднання різних стилів та матеріалів. Приміщення в богемному стилі часто відрізняється використанням яскравих кольорів, натуральних матеріалів, а також різноманітних текстильних виробів.",
+    },
+
+    "electricText":{
+        "en": "Eclectic interior design is a style that combines different elements and styles from different periods and cultures. It arises due to the free combination of various details, styles and eras, creating a unique and individual space without rigid rules.",
+
+        "ua": "Еклектичний дизайн інтер'єру - це стиль, що об'єднує різні елементи та стилі з різних періодів і культур. Він виникає завдяки вільному поєднанню різноманітних деталей, стилів та епох, створюючи унікальний та індивідуальний простір без жорстких правил.",
+    },
+
+    "industrialText":{
+        "en": "Industrial design is a style that arose in connection with the use of industrial and technical elements in the design of premises. It is often associated with spaces that use pipes, metal structures, exposed brick walls and other industrial materials. The main idea of industrial design is to reveal the technical and engineering elements of the room, showing their functionality and raw nature.",
+
+        "ua": "Індустріальний дизайн – це стиль, який виник у зв'язку з використанням промислових та технічних елементів у дизайні приміщень. Він часто асоціюється з просторами, які використовують труби, металеві конструкції, відкриті цегляні стіни та інші промислові матеріали. Основна ідея індустріального дизайну полягає в тому, щоб відкрити технічні та інженерні елементи приміщення, показуючи їхню функціональність та сировинний характер.",
+    },
+
+    "modernText":{
+        "en": "Modern interior design is a reflection of modern trends and styles that define the modern era. The main principles of modern design include simplicity, clean lines, the use of the latest technologies and materials, as well as the rejection of excessive decor.",
+
+        "ua": "Сучасний дизайн інтер'єру є відображенням сучасних тенденцій і стилів, які визначають сучасну епоху. Основні принципи сучасного дизайну включають в себе простоту, чистоту ліній, використання новітніх технологій та матеріалів, а також відмову від надмірного декору.",
+    },
+    "scandinavianText":{
+        "en": "Scandinavian design is a style that emphasizes simplicity, functionality and the use of natural materials. It is characterized by minimalism, neutral colors, natural textures, and an emphasis on convenience and ergonomics in furniture and decor.",
+
+        "ua": "Скандинавський дизайн - це стиль, який підкреслює простоту, функціональність та використання природних матеріалів. Характеризується мінімалізмом, нейтральними кольорами, природними текстурами, та наголосом на зручності та ергономіці у меблях і декорі.",
+    },
+
+    "welcome":{
+        "en": "Welcome to HomeBrilliant, where your search for the perfect home turns into an exciting journey. In our catalog on the website, you will find unique apartments, emphasized by comfort and style, and ready to turn your wishes into reality. Our real estate properties combine high quality with affordability, and our team of experts is ready to provide you with professional support at every stage. Go to our catalog right now to enjoy the wealth of choice and unique offers. We don't just sell apartments, we create long-term relationships, guaranteeing you not only a home, but also a communal space in which you will be happy. Open the door to your new, brilliant future with HomeBrilliant.",
+
+        "ua": "Ласкаво просимо до HomeBrilliant, де ваш пошук ідеального житла перетворюється на захоплюючу подорож. У нашому каталозі на сайті ви знайдете унікальні квартири, підкреслені комфортом та стилем, та готові втілити ваші бажання в реальність. Наші об'єкти нерухомості поєднують високу якість з доступністю, а наша команда експертів готова надати вам професійну підтримку на кожному етапі. Перейдіть в наш каталог прямо зараз, щоб насолодитися багатством вибору та унікальними пропозиціями. Ми не просто продаємо квартири, ми створюємо довгострокові зв'язки, гарантуючи вам не лише будинок, а й комунічне місце, в якому ви будете щасливі. Відкрийте двері у ваше нове, блискуче майбутнє з HomeBrilliant.",
+    },
+    "why":{
+        "en": "Why do people choose us?",
+        "ua": "Чому люди обирають саме нас?",
+    },
+
+    "cause1":{
+        "en": "Unique offers:",
+        "ua": "Унікальні пропозиції:",
+    },
+
+    "cause2":{
+        "en": "Professionalism and support:",
+        "ua": "Професіоналізм та підтримка:",
+    },
+
+    "cause3":{
+        "en": "Long-term relationship:",
+        "ua": "Довгострокові відносини:",
+    },
+
+    "causetext1":{
+        "en": "We are embarking on a journey into a world of unique possibilities together with HomeBrilliant - your reliable guide in the search for the perfect home. Our website catalog invites you to discover special offers that combine sophistication and affordability. You will be able to feel comfort and style in every square meter of your chosen accommodation. Our properties are not just houses, but spaces filled with comfort and elegance. Welcome to the world of HomeBrilliant, where every choice is the way to your own unique space.",
+
+        "ua": "Ми розпочинаємо подорож у світ унікальних можливостей разом з HomeBrilliant - вашим надійним провідником у пошуках ідеального житла. Наш каталог на сайті запрошує вас відкрити для себе особливі пропозиції, які поєднують в собі вишуканість та доступність. Ви зможете відчути комфорт і стиль у кожному квадратному метрі обраного вами помешкання. Наші об'єкти нерухомості - це не просто будинки, а простори, наповнені затишком та елегантністю. Ласкаво просимо у світ HomeBrilliant, де кожен вибір - це шлях до вашого власного неповторного простору.",
+    },
+
+    "causetext2":{
+        "en": "Our team consists of professionals ready to provide you with high-quality service at every stage. We do not limit ourselves to help in choosing housing; we create a hassle-free buying process by providing professional support. Your needs are our priority, and we guarantee you a reliable and comfortable cooperation with HomeBrilliant. Our mission is to make your choice not just a purchase, but a long-term relationship, supporting you every step of the way.",
+
+        "ua": "Наша команда складається з професіоналів, готових надати вам високоякісне обслуговування на кожному етапі. Ми не обмежуємося просто допомогою у виборі житла; ми створюємо безпроблемний процес покупки, забезпечуючи професійну підтримку. Ваші потреби - наш пріоритет, і ми гарантуємо вам надійне і комфортне співпрацювання з HomeBrilliant. Наша місія - зробити ваш вибір не просто покупкою, а створенням довгострокових відносин, підтримуючи вас на кожному кроці.",
+    },
+
+    "causetext3":{
+        "en": "HomeBrilliant doesn't just sell homes; we build long-term partnerships based on mutual understanding and trust. We believe that our clients deserve not only high-quality housing, but also a unique communication environment. After the agreement is concluded, we do not stop our care - we become your reliable partners, ensuring comfort and well-being with our services throughout the life of your new home. Welcome to the HomeBrilliant community, where every customer is part of our family.",
+
+        "ua": "HomeBrilliant не просто продає житло; ми будуємо довгострокові партнерства, що базуються на взаєморозумінні та довірі. Ми віримо, що наші клієнти заслуговують на не тільки високоякісне житло, але й унікальне комунікативне середовище. Після завершення угоди ми не припиняємо нашу турботу - ми стаємо вашими надійними партнерами, забезпечуючи комфорт та благополуччя нашими послугами впродовж усього тривання вашого нового дому. Ласкаво просимо в спільноту HomeBrilliant, де кожен клієнт - це частина нашої родини.",
+    },
+
+    "box4-one":{
+        "en": "HomeBrilliant is the leader among the competition, and here's why. Our company is defined not only as a seller of housing, but also as a creator of individual and elite living spaces. We are ahead of the competition thanks to unique offers that combine luxury, technological innovation and exquisite design. Our properties are true masterpieces in which every element is imbued with style and functionality.",
+
+        "ua": "HomeBrilliant - лідер серед конкурентів, і ось чому. Наша компанія визначається не лише як продавець житла, але і як творець індивідуальних та елітних житлових просторів. Ми випереджаємо конкуренцію завдяки унікальним пропозиціям, які об'єднують в собі розкіш, технологічну інноваційність та вишуканий дизайн. Наші об'єкти нерухомості є справжніми шедеврами, в яких кожен елемент пропитаний стилем та функціональністю.",
+    },
+
+    "box4-two":{
+        "en": "Our team of professionals exceeds expectations, thanks to a high level of expertise and a personal approach to each client. We offer not only sales, but also consultation, support and individual solutions for each buyer. HomeBrilliant is not just a company, it is a lifestyle and a partnership that provides you with not only a beautiful home, but also a sense of privilege and unparalleled quality. Choose us, choose an exceptional future, where every day is a new opportunity to enjoy life in luxury and comfort.",
+
+        "ua": "Наша команда професіоналів перевершує очікування, завдяки високому рівню експертизи та особистому підходу до кожного клієнта. Ми пропонуємо не лише продаж, але й консультації, підтримку та індивідуальні рішення для кожного покупця. HomeBrilliant - це не просто компанія, це стиль життя та партнерство, яке забезпечує вам не тільки прекрасне житло, але й відчуття привілеїв та неперевершеної якості. Обирайте нас, обирайте виняткове майбутнє, де кожен день - це нова можливість насолоджуватися життям в розкіші та комфорті.",
+    },
+
+    "catalog":{
+        "en": "Catalogue",
+        "ua": "Каталог ",
+    }
+
+
+    
+
+};
+
+// translate
+// data-lang-id="under"
--- js/language.js
+++ js/language.js
@@ -1,9 +1,42 @@
+// Функция для сохранения текущего языка в куки
+function saveLanguageToCookies(languageCode) {
+    document.cookie = `currentLanguage=${languageCode}; expires=${new Date(Date.now() + 365 * 24 * 60 * 60 * 1000).toUTCString()}`;
+}
+
+// Функция для получения текущего языка из куки
+function getCurrentLanguageFromCookies() {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.split('=');
+        if (name === 'currentLanguage') {
+            return value;
+        }
+    }
+    return 'en'; // Возвращает "en", если язык не найден
+}
+
+// Общая функция для обновления текста по языку
+function updateTextsByLanguage(languageCode) {
+    const elements = document.querySelectorAll('[data-lang-id]');
+    elements.forEach(element => {
+        const identifier = element.getAttribute('data-lang-id');
+        const translation = langArr[identifier] && langArr[identifier][languageCode];
+        if (translation !== undefined) {
+            element.innerText = translation;
+            console.log(`Updated text for ${identifier} to: ${translation}`);
+        } else {
+            console.error(`Translation not found for ${identifier} in language ${languageCode}`);
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     var languageButton = document.getElementById("languageButton");
     var languageList = document.querySelector(".language-list");
+    var languageButtons = document.querySelectorAll(".language-list .listli a");
 
     languageButton.addEventListener("click", function (event) {
-        event.stopPropagation(); // Prevent the click event from reaching the document
+        event.stopPropagation();
         languageList.classList.toggle("active");
     });
 
@@ -11,19 +44,32 @@ document.addEventListener("DOMContentLoaded", function () {
         languageList.classList.remove("active");
     });
 
-    // Prevent closing the dropdown when clicking inside it
     languageList.addEventListener("click", function (event) {
         event.stopPropagation();
     });
 
     function changeLanguage(languageCode) {
-        // Implement language change logic here
-        console.log("Selected language:", languageCode);
+        console.log("Changing language to:", languageCode);
+        updateTextsByLanguage(languageCode);
         languageList.classList.remove("active");
+
+        // Сохранение текущего языка в куки
+        saveLanguageToCookies(languageCode);
     }
 
-    // Ensure the language list is closed when the document is clicked
     document.addEventListener("click", function () {
         languageList.classList.remove("active");
+    });
+
+    // Получение текущего языка из куки и установка его
+    const currentLanguage = getCurrentLanguageFromCookies();
+    changeLanguage(currentLanguage);
+
+    languageButtons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            var languageCode = button.getAttribute("data-lang-code");
+            changeLanguage(languageCode);
+        });
     });
 });
